68f7cb16987a8cacf82c1ffd6430668c0cec0c13
sullivan986 <2257501878@qq.com>
2022-02-06 22:54:46 +0800

    init

--- ucore/src/main.cpp
+++ ucore/src/main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include "asio.hpp"
+#include "fmt/color.h"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int argc, char const *argv[])
+{
+
+    asio::error_code er;
+
+    asio::io_context ioc;
+
+    asio::ip::tcp::endpoint ep(asio::ip::make_address("0.0.0.0", er), 8080);
+
+    asio::ip::tcp::socket _socket(ioc);
+
+    fmt::print(fmt::emphasis::blink, "program ending!\r123\n");
 }
