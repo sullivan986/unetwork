e7e3c3e988f6e670c12071b907695d109c3c3398
sullivan986 <2257501878@qq.com>
2022-02-17 22:39:16 +0800

    init

--- ucore/src/main.cpp
+++ ucore/src/main.cpp
@@ -1,16 +1,22 @@
+#include <vector>
+
 #include "asio.hpp"
-#include "fmt/color.h"
+#include "fmt/core.h"
 
 int main(int argc, char const *argv[])
 {
-
     asio::error_code er;
 
     asio::io_context ioc;
 
-    asio::ip::tcp::endpoint ep(asio::ip::make_address("0.0.0.0", er), 8080);
+    asio::ip::tcp::endpoint ep(asio::ip::make_address("192.168.0.113", er), 8080);
 
     asio::ip::tcp::socket _socket(ioc);
 
-    fmt::print(fmt::emphasis::blink, "program ending!\r123\n");
+    _socket.connect(ep, er);
+
+    if (!er)
+    {
+        fmt::print("Connecting successful");
+    }
 }
